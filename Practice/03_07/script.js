33ff3567a048d4cf1de7463cfdd49e3aea5ceaaa
Kristian Haugen <kristian-haugen21@live.no>
2025-09-28 18:49:32 +0200

    Practice
    Did some practice, and made my selfe an object in JS, and displayed it in the HTML.

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,25 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+// This is an object:
+
+const gamingPc = {
+  processor: "Amd",
+  grapicsCard: "Nvidia",
+  ram: "32gb",
+  motherBoard: "asus",
+  fans: "8",
+  powerSupplay: "1200w",
+};
+
+//Getting the div:
+const showPcSpec = document.getElementById("gamingPcDiv");
+
+showPcSpec.innerHTML = `  <p><strong>Processor:</strong> ${gamingPc.processor}</p>
+<p><strong>Grapicscard:</strong> ${gamingPc.grapicsCard}</p>
+<p><strong>Ram:</strong> ${gamingPc.ram}</p>
+<p><strong>Motherboard:</strong> ${gamingPc.motherBoard}</p>
+<p><strong>Fans:</strong> ${gamingPc.fans}</p>
+<p><strong>Powersupplay:</strong> ${gamingPc.powerSupplay}</p>
+`;
